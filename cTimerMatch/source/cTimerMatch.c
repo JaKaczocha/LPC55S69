91d29f882a36b6973d69615391cc28bb165b9e08
JaKaczocha <jakub.kaczocha44295@gmail.com>
2024-03-23 01:03:28 +0100

    ctimer match and pwm

---
+++ cTimerMatch/source/cTimerMatch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "LPC55S69_cm33_core0.h"
+#include "fsl_debug_console.h"
+void cbTimerMatch(uint32_t flags) {
+	PRINTF("CT2 INT\r\n");
+}
+/*
+ * @brief Application entry point.
+ */
+int main(void) {
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
+#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
+#endif
+	while(1) {
+	}
+	return 0 ;
+}
